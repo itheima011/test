693c786d659c1962a80403a1c8b745c6fb4c3b90
xiajunfeng <1059833583@qq.com>
2018-11-06 18:39:11 +0800

    提交工程

---
+++ test00/src/main/java/test/test00.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test00 {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+    }
+}
